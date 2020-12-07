fd2dfe34cedc2c5c3e08f4b1ff264689237b1749
enhao25 <larloller@gmail.com>
2020-12-07 21:24:48 +0800

    Styling changes to meal screen

--- Screens/CategoriesScreen.js
+++ Screens/CategoriesScreen.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import { StyleSheet, View, Text, Button, FlatList, TouchableOpacity } from 'react-native'; 
+import { StyleSheet, FlatList } from 'react-native'; 
+import CategoryGridTile from '../Components/CategoryGridTile';
 import { CATEGORIES } from '../Data/Dummy-data'
 
 const CategoriesScreen = props => {
     
     const renderGridItem = (itemData) => {
         return (
-            <TouchableOpacity style={styles.gridItem} onPress={() => {
-                props.navigation.navigate({ routeName: 'CategoryMeals', params: {
-                    categoryId: itemData.item.id
-                }})
-            }}>
-                <View>
-                    <Text>{itemData.item.title}</Text>
-                </View>
-            </TouchableOpacity>
+            <CategoryGridTile 
+                title={itemData.item.title} 
+                color={itemData.item.color}
+                onSelect = {() => {
+                    props.navigation.navigate({ routeName: 'CategoryMeals', params: {
+                        categoryId: itemData.item.id 
+                    }})
+            }} />
         )
     }
 
@@ -28,11 +28,6 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
-    },
-    gridItem: {
-        flex: 1,
-        margin: 15,
-        height: 150
     }
 });
 
